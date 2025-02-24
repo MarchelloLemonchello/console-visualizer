d57926f662d86b93efcfa167cdc7737bb9750bf4
Mark Safronov <marchello.lemonchello@yandex.ru>
2025-02-24 17:21:17 +0300

    settings

---
+++ src/Visualizer.tsx
@@ -0,0 +1,14 @@
+import React, {FC} from "react";
+
+export interface VisualizerProps {
+
+}
+
+export const Visualizer: FC<VisualizerProps> = (props) => {
+  return (
+    <div>
+      Visualizer
+    </div>
+  );
+};
+
